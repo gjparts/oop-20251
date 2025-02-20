afe8086d7d3f8176da68fbd49a6ace21ae5380c8
gjparts <gjparts@gmail.com>
2025-02-20 09:49:57 -0600

    commit 2002202526462

--- 05_asociacion_de_clases/src/main/java/com/mycompany/_asociacion_de_clases/App.java
+++ 05_asociacion_de_clases/src/main/java/com/mycompany/_asociacion_de_clases/App.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany._asociacion_de_clases;
-
-/**
- *
- * @author Gerardo
- */
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Barrio bar1 = new Barrio("Paraiso","Honduras","Cortes","San Pedro Sula");
+        Barrio bar2 = new Barrio("Guamilito","Honduras","Cortes","San Pedro Sula");
+        
+        Direccion dir1 = new Direccion(bar2, "7", "8", "No tiene");
+        Direccion dir2 = new Direccion(bar1, "", "", "10");
+        
+        Empresa cofisa = new Empresa("Compania Financiera S.A.","Credito",dir1,"1234");
+        
+        Cargo car1 = new Cargo("Gerente de IT", 'M', cofisa, 45000);
+        
+        Estudiante es1 = new Estudiante("20012002049", "Gerardo", "Ing. Industrial",
+                                        19, dir2, car1);
+        
+        es1.imprimir();
+        
     }
 }
