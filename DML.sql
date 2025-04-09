54fa212695f9497473599b80586a3060ce0a2abf
gjparts <gjparts@gmail.com>
2025-04-09 11:01:58 -0600

    commit 0904202517634

--- DML.sql
+++ DML.sql
@@ -146,3 +146,109 @@ SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
 FROM Producto
 WHERE Costo >= 30
 ORDER BY Nombre ASC
+--mostrar productos cuyas existencias sean menores a 10
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Existencias < 25
+--tambien puede utilizar operadores logicos como AND y OR
+--mostrar productos cuyo costo este entre 30 y 70
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Costo >= 30 AND Costo <= 70
+--mostrar productos cuyo costo este entre 20 y 30 ó cuyo costo
+--este entre 60 y 100
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE ( Costo >= 20 AND Costo <= 30 ) OR ( Costo >= 60 AND Costo <= 100 )
+--existe un operador llamado BETWEEN que permite obtener
+--registros entre determinados randos numericos o de fecha
+--mostrar productos cuyo costo este entre 30 y 70
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Costo BETWEEN 30 AND 70
+
+--tambien puede usar where en campos calculados
+--mostrar los productos con ganancia >= 5
+SELECT Codigo, Nombre, PrecioVenta-Costo as Ganancia
+FROM Producto
+WHERE PrecioVenta-Costo >= 5
+--se omite el alias en el where
+
+--uso de WHERE con valores NULL
+--productos que tengan su comentario NULL
+SELECT Codigo, Nombre, Comentarios
+FROM Producto
+WHERE Comentarios IS NULL
+--productos que tengan su comentario distinto de NULL
+SELECT Codigo, Nombre, Comentarios
+FROM Producto
+WHERE NOT Comentarios IS NULL
+
+--operador OR
+--productos cuyo precio de venta sea menor a 20 ó mayor igual a 40
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE PrecioVenta < 20 OR PrecioVenta >= 40
+
+--Uso del operador NO ES IGUAL o DIFERENTE DE
+--productos cuyo precio de venta no sea igual a 35
+--forma 1:
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE PrecioVenta != 35
+--forma 2:
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE PrecioVenta <> 35
+--forma 3 (invertir la expresion usando NOT)
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE NOT PrecioVenta = 35
+
+--Uso de WHERE en campos de texto
+--productos cuyo codigo sea PR22
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Codigo = 'PR22'
+--productos cuyo codigo sea PR22, PR01, PR02
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Codigo IN ('PR22','PR01','PR02')
+--IN tambien se puede usar en campos numericos y en subconsultas.
+
+--productos cuyo codigo NO sea PR22, PR01, PR02
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE NOT Codigo IN ('PR22','PR01','PR02')
+
+--uso del operador LIKE en campos de texto
+--productos que lleven la palanbra salud en cualquier parte del comentario
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Comentarios LIKE '%salud%'
+--productos que lleven la letra s en cualquier parte del nombre
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Nombre LIKE '%s%'
+
+--productos que lleven la palabra salud en cualquier parte del comentario
+--y que lleven la letra i en cualquier parte de su nombre
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Comentarios LIKE '%salud%' AND Nombre LIKE '%i%'
+
+--productos cuyo nombre comience con la letra r
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Nombre LIKE 'r%'
+
+--productos cuyo nombre termine con la letra a
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Nombre LIKE '%a'
+
+--productos cuyo codigo comience con PR0
+SELECT ProductoID,Codigo,Nombre,Costo,PrecioVenta,Existencias,Comentarios
+FROM Producto
+WHERE Codigo LIKE 'PR0%'
+
